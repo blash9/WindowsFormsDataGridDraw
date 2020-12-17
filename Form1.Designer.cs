bb01590cc924fce9c1a9111d436275cfb7a1fab3
Root <Root@HOME>
2020-12-18 02:03:12 +0900

    init add

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.列A = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.列B = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.列C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.列A,
+            this.列B,
+            this.列C});
+            this.dataGridView1.Location = new System.Drawing.Point(68, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(661, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            // 
+            // 列A
+            // 
+            this.列A.HeaderText = "Column1";
+            this.列A.Name = "列A";
+            // 
+            // 列B
+            // 
+            this.列B.HeaderText = "Column1";
+            this.列B.Name = "列B";
+            // 
+            // 列C
+            // 
+            this.列C.HeaderText = "Column1";
+            this.列C.Name = "列C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 列A;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 列B;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 列C;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,74 @@ namespace WindowsFormsDataGridDraw
         public Form1()
         {
             InitializeComponent();
+
+            // データグリッドビューに2行分追加
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+
+            dataGridView1.Rows[0].Cells[0].Value = "1行目";
+            dataGridView1.Rows[1].Cells[0].Value = "2行目";
+
+            dataGridView1.Rows[0].Cells[0].ReadOnly = true;
+            dataGridView1.Rows[1].Cells[0].ReadOnly = true;
+          
+            dataGridView1.Rows[0].Cells[1].Value = "文字ですーーーーーーーーーーーーー";
+
+            dataGridView1.Rows[1].Cells[1].Value = "文字ですーーーーーーーーーーーーー";
+
+            dataGridView1.ClearSelection();
+        }
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            //ヘッダー以外のセルで、背景を描画する時
+            if (e.ColumnIndex == 1 && e.RowIndex >= 0 &&
+                (e.PaintParts & DataGridViewPaintParts.Background) ==
+                    DataGridViewPaintParts.Background)
+            {
+                //選択されているか調べ、色を決定する
+                //bColor1が開始色、bColor2が終了色
+                Color bColor1, bColor2;
+                if ((e.PaintParts & DataGridViewPaintParts.SelectionBackground) ==
+                        DataGridViewPaintParts.SelectionBackground &&
+                    (e.State & DataGridViewElementStates.Selected) ==
+                        DataGridViewElementStates.Selected)
+                {
+                    bColor1 = e.CellStyle.SelectionBackColor;
+                    bColor2 = Color.Black;
+                }
+                else
+                {
+                    bColor1 = e.CellStyle.BackColor;
+                    bColor2 = Color.LemonChiffon;
+                }
+
+                using (System.Drawing.Drawing2D.LinearGradientBrush b =
+                    new System.Drawing.Drawing2D.LinearGradientBrush(
+                    e.CellBounds, bColor1, bColor2,
+                    System.Drawing.Drawing2D.LinearGradientMode.Horizontal))
+                {
+                    //セルを塗りつぶす
+                    e.Graphics.FillRectangle(b, e.CellBounds);
+                }
+
+                //e.Graphics.DrawString("テスト", new Font("Tahoma", 10.5f), Brushes.Black, 5, 5);
+
+                ////背景以外が描画されるようにする
+                DataGridViewPaintParts paintParts =
+                   e.PaintParts & ~DataGridViewPaintParts.Background;
+                //セルを描画する
+                e.Paint(e.ClipBounds, paintParts);
+
+
+                e.Graphics.DrawString("オーバーレイ", e.CellStyle.Font,
+                    Brushes.Crimson, e.CellBounds.X + 20,
+                    e.CellBounds.Y + 2, StringFormat.GenericDefault);
+
+                //描画が完了したことを知らせる
+                e.Handled = true;
+            }
+
         }
     }
 }
